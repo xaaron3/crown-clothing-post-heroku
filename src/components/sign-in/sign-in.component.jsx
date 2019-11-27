b0abd6fe0cfb95a5b73fc8f87877eb6295d1830b
aaron <xakong@outlook.com>
2019-11-26 19:52:29 -0500

    user / cart sagas, sign in sign up w google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+import {
+   googleSignInStart,
+   emailSignInStart
+} from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -11,26 +17,23 @@ class SignIn extends React.Component {
       password: ''
    };
 
-   handleSubmit = async (event) => {
+   handleSubmit = async event => {
       event.preventDefault();
-
+      const { emailSignInStart } = this.props;
       const { email, password } = this.state;
 
-      try {
-         await auth.signInWithEmailAndPassword(email, password);
-         this.setState({ email: '', password: '' });
-      } catch (error) {
-         console.log(error);
-      }
+      emailSignInStart(email, password);
    };
 
-   handleChange = (e) => {
+   handleChange = e => {
       const { value, name } = e.target;
 
       this.setState({ [name]: value });
    };
 
    render() {
+      const { googleSignInStart } = this.props;
+
       return (
          <div className='sign-in'>
             <h2>I already have an account</h2>
@@ -55,7 +58,11 @@ class SignIn extends React.Component {
                />
                <div className='buttons'>
                   <CustomButton type='submit'>Sign In</CustomButton>
-                  <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                  <CustomButton
+                     type='button'
+                     onClick={googleSignInStart}
+                     isGoogleSignIn
+                  >
                      Sign In With Google
                   </CustomButton>
                </div>
@@ -65,4 +72,10 @@ class SignIn extends React.Component {
    }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+   googleSignInStart: () => dispatch(googleSignInStart()),
+   emailSignInStart: (email, password) =>
+      dispatch(emailSignInStart({ email, password }))
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -27,8 +27,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
          return {
             ...state,
             cartItems: state.cartItems.filter(
-               (cartItem) => cartItem.id !== action.payload.id
+               cartItem => cartItem.id !== action.payload.id
             )
+         };
+      case CartActionTypes.CLEAR_CART:
+         return {
+            ...state,
+            cartItems: []
          };
       default:
          return state;
